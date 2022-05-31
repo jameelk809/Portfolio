b2f53ab0d185630f8187791e3fdf70765d1ea68d
Kamran <jameelk809@gmail.com>
2022-05-31 09:08:20 +0530

    Updates

---
+++ precache-manifest.3d40e5a2bae251d14cc33b6e7e7881c2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6be1ebb958e483e4a64a04f575b1c4f0",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "3e9a33c745314dd0be34",
+    "url": "/Portfolio/static/css/main.73590d41.chunk.css"
+  },
+  {
+    "revision": "a6c8a7d4cb3bf2bfea6d",
+    "url": "/Portfolio/static/js/2.ffab706f.chunk.js"
+  },
+  {
+    "revision": "9efecc6e366a89e935d3722c67df4853",
+    "url": "/Portfolio/static/js/2.ffab706f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e9a33c745314dd0be34",
+    "url": "/Portfolio/static/js/main.c84e21cf.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/Portfolio/static/js/main.c84e21cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87bcb24a22a073d38476",
+    "url": "/Portfolio/static/js/runtime-main.d4446722.js"
+  },
+  {
+    "revision": "4cb23d02b5ca8a1fde687627b0a218cb",
+    "url": "/Portfolio/static/media/kamran.4cb23d02.jpg"
+  }
+]);
